b9939d332ab20390047b94eaeccc1ee2f1c8bfbb
Tim La <tlamarketing111@gmail.com>
2022-11-07 14:09:53 -0500

    created main page component

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Main } from './pages/main'
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
