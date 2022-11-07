0758441205dec7a28c7717e4cc2144b877b6a8b7
Tim La <tlamarketing111@gmail.com>
2022-11-07 14:10:46 -0500

    created login component

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Main } from './pages/main'
+import { Login } from './pages/login'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
