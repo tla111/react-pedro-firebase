c3da41e8761c119d74788ad22c750d396a4eda56
Tim La <tlamarketing111@gmail.com>
2022-11-07 14:24:31 -0500

    able to navigate to homepage when loggin

--- front_end/src/pages/login.tsx
+++ front_end/src/pages/login.tsx
@@ -1,10 +1,14 @@
 import { auth, provider } from "../config/firebase"
 import { signInWithPopup } from "firebase/auth"
+import { useNavigate } from "react-router-dom"
 
 export const Login = () => {
+    const navigate = useNavigate()
+
     const signInWithGoogle = async () => {
         const result = await signInWithPopup(auth, provider)
         console.log(result);
+        navigate('/')
 
     }
 
