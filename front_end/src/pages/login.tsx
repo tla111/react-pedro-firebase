be99324f5aaffca18dcf78ebc786464568fa39f2
Tim La <tlamarketing111@gmail.com>
2022-11-07 14:23:18 -0500

    able to authenticate with google

--- front_end/src/pages/login.tsx
+++ front_end/src/pages/login.tsx
@@ -2,10 +2,16 @@ import { auth, provider } from "../config/firebase"
 import { signInWithPopup } from "firebase/auth"
 
 export const Login = () => {
+    const signInWithGoogle = async () => {
+        const result = await signInWithPopup(auth, provider)
+        console.log(result);
+
+    }
+
     return (
         <div>
             <p>Sign In with Google</p>
-            <button>Sign In with Google</button>
+            <button onClick={signInWithGoogle}>Sign In with Google</button>
         </div>
     )
 }
