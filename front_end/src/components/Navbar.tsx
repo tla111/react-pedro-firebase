84bd54107e34d306baaca6f4bdb430d57d7f7bfe
Tim La <tlamarketing111@gmail.com>
2022-11-07 14:35:54 -0500

    added css to navbar

--- front_end/src/components/Navbar.tsx
+++ front_end/src/components/Navbar.tsx
@@ -1,17 +1,25 @@
 import { Link } from "react-router-dom"
 import { auth } from "../config/firebase"
 import { useAuthState } from 'react-firebase-hooks/auth'
+import "../App.css"
 
 export const Navbar = () => {
     const [user] = useAuthState(auth);
 
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
-            <div>
-                <p>{user?.displayName}</p>
-                {/* <img src={user?.photoURL || ""} width="100" height="100" alt="user profile" /> */}
+        <div className="navbar">
+            <div className="links">
+                <Link to="/"> Home </Link>
+                <Link to="/login"> Login </Link>
+            </div>
+            <div className="user">
+                {user && (
+                    <>
+                        <p> {user?.displayName} </p>
+                        {/* <img src={user?.photoURL || ""} width="20" height="20" /> */}
+                        {/* <button onClick={signUserOut}> Log Out</button> */}
+                    </>
+                )}
             </div>
         </div>
     )
