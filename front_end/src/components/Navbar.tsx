6474c6f4ee56a1f81d6e8336c51d7cbe2d7103fb
Tim La <tlamarketing111@gmail.com>
2022-11-07 14:38:39 -0500

    able to logout user

--- front_end/src/components/Navbar.tsx
+++ front_end/src/components/Navbar.tsx
@@ -1,11 +1,14 @@
-import { Link } from "react-router-dom"
-import { auth } from "../config/firebase"
-import { useAuthState } from 'react-firebase-hooks/auth'
-import "../App.css"
+import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
 export const Navbar = () => {
     const [user] = useAuthState(auth);
 
+    const signUserOut = async () => {
+        await signOut(auth);
+    };
     return (
         <div className="navbar">
             <div className="links">
@@ -16,11 +19,11 @@ export const Navbar = () => {
                 {user && (
                     <>
                         <p> {user?.displayName} </p>
-                        {/* <img src={user?.photoURL || ""} width="20" height="20" /> */}
-                        {/* <button onClick={signUserOut}> Log Out</button> */}
+                        <img src={user?.photoURL || ""} width="20" height="20" />
+                        <button onClick={signUserOut}> Log Out</button>
                     </>
                 )}
             </div>
         </div>
-    )
-}
+    );
+};
