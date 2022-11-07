bb955390e0acdcf2a322163abfc963af3a9399a3
Tim La <tlamarketing111@gmail.com>
2022-11-07 14:17:43 -0500

    installed firebase

---
+++ front_end/src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDpVjfm-FNinV8D6QYCLkrFnU9ZpM18AjM",
+    authDomain: "react-course-pedro.firebaseapp.com",
+    projectId: "react-course-pedro",
+    storageBucket: "react-course-pedro.appspot.com",
+    messagingSenderId: "138348999668",
+    appId: "1:138348999668:web:4535458908a2221ac6ffa1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
