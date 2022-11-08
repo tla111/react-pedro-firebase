6ce8136022d43591f8d96a246ff2a40d1a3acaa3
Tim La <tlamarketing111@gmail.com>
2022-11-08 10:18:52 -0500

    installed dependecies

--- front_end/src/pages/create-post/create-form.tsx
+++ front_end/src/pages/create-post/create-form.tsx
@@ -1,3 +1,5 @@
 export const CreateForm = () => {
-
+    return (
+        <form></form>
+    )
 }
--- front_end/src/pages/create-post/create-post.tsx
+++ front_end/src/pages/create-post/create-post.tsx
@@ -3,7 +3,7 @@ import { CreateForm } from "./create-form"
 export const CreatePost = () => {
     return (
         <div>
-
+            <CreateForm />
         </div>
     )
 }
