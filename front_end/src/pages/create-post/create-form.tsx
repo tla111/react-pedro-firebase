9630b0d2de1a58e5c3d456417f67614d19d123be
Tim La <tlamarketing111@gmail.com>
2022-11-08 10:23:43 -0500

    creating schema for post

--- front_end/src/pages/create-post/create-form.tsx
+++ front_end/src/pages/create-post/create-form.tsx
@@ -2,10 +2,14 @@ import { useForm } from "react-hook-form";
 import * as yup from "yup";
 
 export const CreateForm = () => {
+    const schema = yup.object().shape({
+        title: yup.string().required("You must add a title."),
+        description: yup.string().required("You must add a description."),
+    })
     return (
         <form>
             <input placeholder="Title..." />
-            <input placeholder="Description..." />
+            <textarea placeholder="Description..." />
             <input type="submit" />
         </form>
     )
