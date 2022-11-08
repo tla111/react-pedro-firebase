1aa269d75d700ee0cbc73456994ee40b7fbac95f
Tim La <tlamarketing111@gmail.com>
2022-11-08 10:27:45 -0500

    able to store title & description

--- front_end/src/pages/create-post/create-form.tsx
+++ front_end/src/pages/create-post/create-form.tsx
@@ -1,15 +1,26 @@
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export const CreateForm = () => {
     const schema = yup.object().shape({
         title: yup.string().required("You must add a title."),
         description: yup.string().required("You must add a description."),
     })
+
+    const { register, handleSubmit } = useForm({
+        resolver: yupResolver(schema)
+    })
+
+    const onCreatePost = (data: any) => {
+        console.log(data);
+
+    }
+
     return (
-        <form>
-            <input placeholder="Title..." />
-            <textarea placeholder="Description..." />
+        <form onSubmit={handleSubmit(onCreatePost)}>
+            <input placeholder="Title..." {...register("title")} />
+            <textarea placeholder="Description..." {...register("description")} />
             <input type="submit" />
         </form>
     )
