f9e92f2f348d7ab2d53b9580b8bc1e5880d9b6b2
Tim La <tlamarketing111@gmail.com>
2022-11-09 12:21:10 -0500

    assign type

--- front_end/src/pages/main.tsx
+++ front_end/src/pages/main.tsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import { getDocs, collection } from 'firebase/firestore'
 import { db } from '../config/firebase'
 
+interface Post {
+    id: string;
+    userId: string;
+    title: string;
+    username: string;
+    description: string;
+}
+
 export const Main = () => {
-    const [postsList, setPostsList] = useState(null)
+    const [postsList, setPostsList] = useState<Post[] | null>(null)
     const postsRef = collection(db, "posts")
 
     const getPosts = async () => {
         const data = await getDocs(postsRef)
-        console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        setPostsList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]);
 
     }
 
