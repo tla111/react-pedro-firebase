96cf05c7a1bd451e0a161bf4396821e48ce205a9
Tim La <tlamarketing111@gmail.com>
2022-11-09 12:19:12 -0500

    able to retrieve data from firebase

--- front_end/src/pages/main.tsx
+++ front_end/src/pages/main.tsx
@@ -1,8 +1,18 @@
+import { useState } from 'react'
 import { getDocs, collection } from 'firebase/firestore'
 import { db } from '../config/firebase'
 
 export const Main = () => {
+    const [postsList, setPostsList] = useState(null)
     const postsRef = collection(db, "posts")
+
+    const getPosts = async () => {
+        const data = await getDocs(postsRef)
+        console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+
+    }
+
+    getPosts()
 
     return (
         <div>
