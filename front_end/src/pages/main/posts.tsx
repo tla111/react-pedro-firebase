1faab9fbbd3f1d884706c3e07bb13ed5b87e4c49
Tim La <tlamarketing111@gmail.com>
2022-11-09 12:49:54 -0500

    specify type of likeAmount

--- front_end/src/pages/main/posts.tsx
+++ front_end/src/pages/main/posts.tsx
@@ -1,5 +1,5 @@
 import { addDoc, getDocs, collection, query, where } from 'firebase/firestore'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, db } from '../../config/firebase'
 import { Post as IPost } from './main'
@@ -12,13 +12,15 @@ export const Post = (props: Props) => {
     const { post } = props
     const [user] = useAuthState(auth)
 
+    const [likeAmount, setLikeAmount] = useState<number | null>(null)
+
     const likesRef = collection(db, "likes")
 
     const likesDoc = query(likesRef, where("postId", "==", post.id))
 
     const getLikes = async () => {
         const data = await getDocs(likesDoc)
-        console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        setLikeAmount(data.docs.length)
     }
 
     const addLike = async () => {
@@ -40,7 +42,7 @@ export const Post = (props: Props) => {
             <div className="footer">
                 <p>@{post.username}</p>
                 <button onClick={addLike}>Like</button>
-                <p>Likes: { }</p>
+                <p>Likes: {likeAmount}</p>
             </div>
         </div>
     )
